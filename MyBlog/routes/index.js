b86f65afb54bcdea1e5cdbef68877a464243a9dd
ChenZheng <xiaobai417sos@163.com>
2017-05-14 01:06:49 +0800

    第三次提

--- MyBlog/routes/index.js
+++ MyBlog/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('这是第一个目录', { title: 'Express框架展示' });
 });
 
 module.exports = router;
